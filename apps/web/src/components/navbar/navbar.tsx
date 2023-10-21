fb461e0520ce9a3004b34fca515a195f4f3ee0ba
Kasidis Chantharojwong <me@ckasidis.com>
2023-10-21 18:06:13 +0800

    chore: initial commit

---
+++ apps/web/src/components/navbar/navbar.tsx
@@ -0,0 +1,104 @@
+"use client";
+
+import { UnstyledButton, Tooltip, Title, rem } from "@mantine/core";
+import {
+  IconHome2,
+  IconGauge,
+  IconDeviceDesktopAnalytics,
+  IconFingerprint,
+  IconCalendarStats,
+  IconUser,
+  IconSettings,
+} from "@tabler/icons-react";
+import Image from "next/image";
+import Link from "next/link";
+import { useState } from "react";
+
+import classes from "./navbar.module.css";
+
+const mainLinksMockdata = [
+  { icon: IconHome2, label: "Home" },
+  { icon: IconGauge, label: "Dashboard" },
+  { icon: IconDeviceDesktopAnalytics, label: "Analytics" },
+  { icon: IconCalendarStats, label: "Releases" },
+  { icon: IconUser, label: "Account" },
+  { icon: IconFingerprint, label: "Security" },
+  { icon: IconSettings, label: "Settings" },
+];
+
+const linksMockdata = [
+  "Security",
+  "Settings",
+  "Dashboard",
+  "Releases",
+  "Account",
+  "Orders",
+  "Clients",
+  "Databases",
+  "Pull Requests",
+  "Open Issues",
+  "Wiki pages",
+];
+
+export function Navbar() {
+  const [active, setActive] = useState("Releases");
+  const [activeLink, setActiveLink] = useState("Settings");
+
+  const mainLinks = mainLinksMockdata.map((link) => (
+    <Tooltip
+      label={link.label}
+      position="right"
+      withArrow
+      transitionProps={{ duration: 0 }}
+      key={link.label}
+    >
+      <UnstyledButton
+        onClick={() => setActive(link.label)}
+        className={classes.mainLink}
+        data-active={link.label === active || undefined}
+      >
+        <link.icon style={{ width: rem(22), height: rem(22) }} stroke={1.5} />
+      </UnstyledButton>
+    </Tooltip>
+  ));
+
+  const links = linksMockdata.map((link) => (
+    <Link
+      className={classes.link}
+      data-active={activeLink === link || undefined}
+      href="#"
+      onClick={(event) => {
+        event.preventDefault();
+        setActiveLink(link);
+      }}
+      key={link}
+    >
+      {link}
+    </Link>
+  ));
+
+  return (
+    <nav className={classes.navbar}>
+      <div className={classes.wrapper}>
+        <div className={classes.aside}>
+          <div className={classes.logo}>
+            <Image
+              src="/blockdutch.svg"
+              width={28}
+              height={28}
+              alt="BlockDutch"
+            />
+          </div>
+          {mainLinks}
+        </div>
+        <div className={classes.main}>
+          <Title order={4} className={classes.title}>
+            {active}
+          </Title>
+
+          {links}
+        </div>
+      </div>
+    </nav>
+  );
+}
